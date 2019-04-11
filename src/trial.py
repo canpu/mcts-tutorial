16e93b68a10b3515179281e733724d4b7f3ba38e
Can Pu <pucan@mit.edu>
2019-04-11 18:18:36 -0400

    Added example Gomoku game, a suggested strategy for the black to win, and corresponding images

--- src/trial.py
+++ src/trial.py
@@ -1,11 +1,19 @@
 from gomoku import *
 from mcts import *
 
-state = GomokuState(use_default_heuristics=True)
-state.go((GomokuState.board_size // 2, GomokuState.board_size // 2))
-state.go((GomokuState.board_size // 2 + 1, GomokuState.board_size // 2))
-state.go((GomokuState.board_size // 2, GomokuState.board_size // 2 + 1))
-state.go((GomokuState.board_size // 2 + 1, GomokuState.board_size // 2 + 1))
+# We create an example game; in this game, the black has taken great advantage
+initial_state = GomokuState(use_default_heuristics=True)
+initial_state.go((3, 5)).go((3, 6)).go((4, 6)).go((5, 7)).go((5, 5)) \
+    .go((6, 4)).go((4, 5)).go((6, 5)).go((4, 4)).go((4, 7)).go((6, 6)) \
+    .go((7, 7)).go((2, 5)).go((1, 5)).go((3, 3)).go((2, 2)).go((6, 7)) \
+    .go((3, 4))
+initial_state.visualize()
+
+final_state = initial_state.__copy__()
+final_state.go((4, 3)).go((4, 2)).go((5, 3)).go((6, 3)).go((6, 2)).go((2, 6)) \
+    .go((7, 1))
+final_state.visualize()
+
 mcts = MonteCarloSearchTree(samples=200)
-action = mcts.search_for_action(state)
+action = mcts.search_for_action(initial_state)
 print(action)
